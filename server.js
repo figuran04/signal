a2337ce59127bb30998941e82905812b674cd328
figuran04 <dikaelsaputra2406@gmail.com>
2026-05-09 23:21:07 +0700

    add start

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const wss = new WebSocket.Server({
 
 const rooms = {};
 
-console.log("WebSocket server running on ws://localhost:3001");
+console.log("WebSocket server running on :3001");
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
